3c96cf53d8bb3d80b06e9c8ceabfe85f56299b39
Vivesh kumar <vivesh103@gmail.com>
2025-01-07 15:36:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,5 +26,5 @@ const divide = (arr, n) => {
 	
 };
 
-// const n = prompt("Enter n: ");
-// alert(JSON.stringify(divide(arr, n)));
+const n = prompt("Enter n: ");
+alert(JSON.stringify(divide(arr, n)));
